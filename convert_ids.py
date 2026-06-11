e3325b19018b77e7e299ecc0c95004149d532465
yashasCC <155967093+yashasCC@users.noreply.github.com>
2026-06-11 15:27:27 +0530

    Update convert_ids.py

--- convert_ids.py
+++ convert_ids.py
@@ -22,7 +22,7 @@ def convert_ids(input_path, output_path, start_index=1):
         )
 
         if strict_uuid:
-            replacement = f'"id": "${{P_randomID{counter}}}"'
+            replacement = f'"id": "${{P_RandomID{counter}}}"'
             counter += 1
             return replacement
         else:
